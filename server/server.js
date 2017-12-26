a73948373c1b1423b05054857ca750b516e7680d
AkashGupta13 <bpfsakash@gmail.com>
2017-12-26 19:54:14 +0530

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
-app.get('/',function (request,response,next){
-  response.send('Server is up and running!');
+io.on('connection',function(socket){
+  console.log('new user connected');
+  socket.on('disconnect',function(socket){
+    console.log('user disconnected');
+  });
 });
 
-app.listen(port,function(){
+
+
+server.listen(port,function(){
   console.log('server is up on port',port);
 });
