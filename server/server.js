a1d891f1938af4a8166eb887ee1ed803e55be909
AkashGupta13 <bpfsakash@gmail.com>
2017-12-26 19:26:22 +0530

    Initial Commit'

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -12,6 +12,6 @@ app.get('/',function (request,response,next){
   response.send('Server is up and running!');
 });
 
-app.listen(3000,function(){
+app.listen(port,function(){
   console.log('server is up on port',port);
 });
